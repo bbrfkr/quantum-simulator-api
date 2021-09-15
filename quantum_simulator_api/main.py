e4660b33f1a51e27f89254e7379123da9cf998be
bbrfkr <bbrfkr@gmail.com>
2021-09-15 11:55:16 +0000

    wip

--- quantum_simulator_api/main.py
+++ quantum_simulator_api/main.py
@@ -5,12 +5,15 @@ from math import sqrt
 from typing import Dict, List
 
 import quantum_simulator.channel.channel as qc
+import quantum_simulator.channel.state as qs
+import quantum_simulator.channel.registers as qr
 from fastapi import FastAPI, HTTPException
 from fastapi_contrib.db.models import MongoDBModel
 from fastapi_contrib.db.utils import setup_mongodb
 from pydantic import Field
 from quantum_simulator.base.observable import Observable
 from quantum_simulator.base.pure_qubits import PureQubits
+from quantum_simulator.base.qubits import Qubits
 from quantum_simulator.base.time_evolution import TimeEvolution
 from quantum_simulator.channel.transformer import (
     ObserveTransformer,
@@ -152,25 +155,23 @@ async def initialize_state_of_channel(id: int):
                 )
         except Exception:
             raise HTTPException(
-                status_code=404, detail="cannot convert matrix to transformer"
+                status_code=400, detail="cannot convert matrix to transformer"
             )
         init_transformers.append(qc_transformer)
-        logger.info(init_transformers)
 
+    qc_channel = qc.Channel(
+        qubit_count=channel.qubit_count,
+        register_count=channel.register_count,
+        init_transformers=init_transformers,
+    )
     try:
-        qc_channel = qc.Channel(
-            qubit_count=channel.qubit_count,
-            register_count=channel.register_count,
-            init_transformers=init_transformers,
-        )
         qc_channel.initialize()
     except Exception:
         raise HTTPException(status_code=400, detail="cannot initialize channel")
 
     qubits = qc_channel.states[0].qubits.matrix.astype(str).tolist()
-    state_id = await State(
-        qubits=qubits, registers=qc_channel.states[0].registers.values
-    ).save()
+    registers = qc_channel.states[0].registers.values
+    state_id = await State(qubits=qubits, registers=registers).save()
     try:
         await Channel.update_one(
             filter_kwargs={"id": channel.id},
@@ -188,25 +189,65 @@ async def initialize_state_of_channel(id: int):
 
 
 @app.put("/channel/{id}/transform", response_model=Dict[str, str])
-async def apply_transformer_to_channel(id: int, transformer_id: int):
+async def apply_transformer_to_channel(id: int, transformer_id: int, register_index: int):
+    # get channel
     channel = await Channel.get(id=id)
     if not channel:
         raise HTTPException(
             status_code=404, detail=f"channel with id '{id}' is not found"
         )
+    qc_channel = qc.Channel(
+        qubit_count=channel.qubit_count,
+        register_count=channel.register_count,
+        init_transformers=[],
+    )
 
+    # get transformer. then set transformer to channel
     transformer = await Transformer.get(id=transformer_id)
     if not transformer:
         raise HTTPException(
             status_code=404,
             detail=f"transformer with id '{transformer_id}' is not found",
         )
-
+    try:
+        if transformer.type == TransformerType.OBSERVE:
+            qc_transformer = ObserveTransformer(
+                Observable([list(map(complex, row)) for row in transformer.matrix])
+            )
+        elif transformer.type == TransformerType.TIMEEVOLVE:
+            qc_transformer = TimeEvolveTransformer(
+                TimeEvolution(
+                    [list(map(complex, row)) for row in transformer.matrix]
+                )
+            )
+    except Exception:
+        raise HTTPException(
+            status_code=400, detail="cannot convert matrix to transformer"
+        )
     channel.transformer_ids.append(transformer_id)
 
-    # TODO: calculate applied state
-    state = State(qubits=[], registers=[])
-    state_id = await state.save()
+    # get previous state. then set the state to channel
+    try:
+        pre_state = await State.get(id=channel.state_ids[-1])
+    except Exception:
+        raise HTTPException(
+            status_code=400,
+            detail=f"this channel is not initialized",
+        )
+    qc_registers = qr.Registers(len(pre_state.regesters))
+    for index, value in enumerate(pre_state.regesters):
+        qc_registers.put(index, value)
+    qc_qubits = Qubits([list(map(complex, row)) for row in pre_state.qubits])
+    qc_state = qs.State(qc_qubits, qc_registers)
+    qc_channel.states = [qc_state]
+    
+    # transform!!
+    qc_channel.transform(qc_transformer, register_index)
+
+    # append post state to channel
+    post_qubits = qc_channel.states[-1].qubits.matrix.astype(str).tolist()
+    post_registers = qc_channel.states[-1].registers.values
+    state_id = await State(qubits=post_qubits, post_registers=registers).save()
     channel.state_ids.append(state_id)
 
     try:
